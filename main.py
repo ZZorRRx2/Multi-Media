ec0e979abc86c4e491412a57a97ad17cbcd5929d
ZZorRRx2 <matthewto2is@gmail.com>
2024-03-13 14:51:51 +0000

    logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #imports necssary modules
 import discord
 import os
+import logging
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -9,7 +10,7 @@ from dotenv import load_dotenv
 #Here we are listening to all events that can happen
 intents = discord.Intents.all() #Initalise an instance of intents  with all intents enabled
 client = discord.Client(intents=intents) #Creates a Discord client instance with the defined intents
-bot = command.Bot(command_prefix='!', intents=intents) #Creates a bot with the command prefix set to !
+bot = commands.Bot(command_prefix='mm! ', intents=intents) #Creates a bot with the command prefix set to !
 
 #Declare that the bot is ready and running
 @bot.event #Marks event
@@ -17,12 +18,31 @@ async def on_ready(): #The event when the bot is ready and logged in
 	print(f'Logged in as {bot.user.name}') #Our message telling all that the bot is ready
 
 #Set the commands
-@bot.command()
-async def greet(ctx):
-	resposne = 'Hello'
+@bot.command() #This is a command
+async def greet(ctx): #the "greet" is a response for !greet. ctx allows the context of the command to be displayed where the called command occured
+	response = 'Hello' #this holds the string for the response
 	await ctx.send(response)
 
 @bot.command()
 async def list_command(ctx):
-	resonse = 'A response'
+	response = 'A response'
 	await ctx.send (response)
+
+#Events
+@bot.event
+async def on_command_error(ctx, error):
+	error_message = f'Error occured while processing command: {error}'
+	logging.error(error_message)
+	await ctx.send(error_message)
+
+#Retrieve token from .env file
+load_dotenv() #Takes our token from the .env file
+bot.run(os.getenv('TOKEN')) #Runs the discord bot using our Token
+
+#Set the logging settings
+logging.basicConfig(level=logging.INFO,
+	format='[%(asctime)s] [%(levelname)s]: %(messages)s',
+	handlers=[
+		logging.FileHandler('bot.log'), #saves logs to a file
+		logging.StreamHandler() 	#Displays logs in the console
+])
